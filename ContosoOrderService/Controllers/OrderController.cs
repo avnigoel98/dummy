ffff554facce036434ecc06d7b793faf9267b479
avnigoel98 <avni.goel@insight.com>
2025-04-15 00:16:45 +0530

    added

---
+++ ContosoOrderService/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoOrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "This is the Order Service (dummy)" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrderById(int id)
+        {
+            return Ok(new { orderId = id, status = "Confirmed" });
+        }
+    }
+}
